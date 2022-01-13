9ed4ba495668453a7716baa8f3b8c8aee810f167
OpenFileVersion <97646548+OpenFileVersion@users.noreply.github.com>
2022-01-13 10:21:53 +0800

    Open File Version

--- builtin/mainmenu/tab_about.lua
+++ builtin/mainmenu/tab_about.lua
@@ -117,6 +117,8 @@ return {
 			buildCreditList(previous_core_developers) .. ",,," ..
 			"#FFFF00," .. fgettext("Previous Contributors") .. ",," ..
 			buildCreditList(previous_contributors) .. "," ..
+                        "#FFFF00," .. fgettext("Debug Info") .. "," .. 
+                        buildCreditList("debug_information") .. "," .. 
 			";1]"
 
 		-- Render information
